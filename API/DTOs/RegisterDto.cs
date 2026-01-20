42d3fc1b125f1e43471dc39fba46ded6a2d229e8
Li, Yuxuan <lyx776239423@gmail.com>
2026-01-20 22:38:49 +0800

    add

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,6 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs;
 
+/**
+ * RegisterDto
+ * 
+ * @property {string} Email - The email of the user.
+ * @property {string} DisplayName - The display name of the user.
+ * @property {string} Password - The password of the user.
+ */
 public class RegisterDto
 {
     [Required] public string Email { get; set; } = "";
